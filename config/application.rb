7487c988b659645191cb01c1022ed285532e8380
yutakanagashima <holafuki@gmail.com>
2018-06-03 03:15:49 +0000

    toukousyusei

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,10 @@ module TodoApp
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    
+    # 認証トークンをremoteフォームに埋め込む
+    config.action_view.embed_authenticity_token_in_remote_forms = true
+    
     config.i18n.default_locale = :ja
   end
 end
