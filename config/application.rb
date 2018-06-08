95390b3447d95aa635c7588c11cafe74c6976b0a
yutakanagashima <holafuki@gmail.com>
2018-06-08 07:56:37 +0000

    komakaitokorosyuusei1

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ module TodoApp
     # -- all .rb files in that directory are automatically loaded.
     
     # 認証トークンをremoteフォームに埋め込む
-    config.action_view.embed_authenticity_token_in_remote_forms = true
+    #config.action_view.embed_authenticity_token_in_remote_forms = true
     
     config.i18n.default_locale = :ja
   end
